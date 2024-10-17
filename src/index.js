d564db8f721049938b90831bd78e57ad81582e07
Johnathan Rafael <nahtanhoj.555@gmail.com>
2024-10-17 07:44:56 -0300

    Corrigindo explicação das funções

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // A aplicação não considera a entrada pelo usuário, para testar resultados basta alterar 
-// os parâmetros na chamada da função calculateResultRanked(0,0) na linha 28
+// os parâmetros na chamada da função calculateResultRanked(0,0) na linha 30
 
 // Função para calcular a quantidade de vitórias
 function calculateResultRanked(victory, loses){
@@ -25,7 +25,6 @@ function verifyResult(amountVictory){
     }
 }
 
-// função que 
 function main(){
     // mudar os parâmetros aqui
     let amountWins = calculateResultRanked(0,5);
